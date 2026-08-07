eb4eb25f2b7a85b33744ed917950d4214de17b14
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-08-07 13:07:41 +0200

    Initial commit: Angular 21 project setup

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, resource-reservation-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, resource-reservation-frontend',
+    );
   });
 });
