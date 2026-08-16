0f1ddaf7f107d5f282b713c73e26872442f5b798
Krzysztof Lech <krzysztof.lech02@gmail.com>
2026-08-16 14:37:04 +0200

    feat: add collapsible search field to public resource list

--- src/app/features/resources/resource-list/resource-list.ts
+++ src/app/features/resources/resource-list/resource-list.ts
@@ -1,20 +1,27 @@
 import { Component, inject, signal, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Router } from '@angular/router';
 import { ResourceService } from '../../../core/services/resource.service';
 import { ResourceReadDto } from '../../../models/resource.model';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-resource-list',
   imports: [
+    FormsModule,
     MatCardModule,
     MatChipsModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatProgressSpinnerModule
   ],
   templateUrl: './resource-list.html',
@@ -28,8 +35,21 @@ export class ResourceList implements OnInit {
   loading = signal(true);
   errorMessage = signal<string | null>(null);
 
+  searchOpen = signal(false);
+  searchQuery = '';
+  private searchSubject = new Subject<string>();
+
   ngOnInit(): void {
-    this.resourceService.getAll().subscribe({
+    this.loadResources();
+
+    this.searchSubject.pipe(debounceTime(300)).subscribe(() => {
+      this.loadResources();
+    });
+  }
+
+  loadResources(): void {
+    this.loading.set(true);
+    this.resourceService.getAll(this.searchQuery).subscribe({
       next: (data) => {
         this.resources.set(data.filter(r => r.isAvailable));
         this.loading.set(false);
@@ -39,6 +59,18 @@ export class ResourceList implements OnInit {
         this.loading.set(false);
       }
     });
+  }
+
+  onSearchChange(): void {
+    this.searchSubject.next(this.searchQuery);
+  }
+
+  toggleSearch(): void {
+    this.searchOpen.update(open => !open);
+    if (!this.searchOpen()) {
+      this.searchQuery = '';
+      this.loadResources();
+    }
   }
 
   viewDetails(id: string): void {
